c5a626088ab73daf87e4dd41715260bf9d45d435
kklldog <kklldog@qq.com>
2020-05-12 20:21:14 +0800

    添加项目文件。

---
+++ BlazorServerDemo/Data/IStudentRepository.cs
@@ -0,0 +1,21 @@
+﻿using BlazorServerDemo.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorServerDemo.Data
+{
+    public interface IStudentRepository
+    {
+        List<Student> List();
+
+        Student Get(int id);
+
+        bool Add(Student student);
+
+        bool Update(Student student);
+
+        bool Delete(int id);
+    }
+}
